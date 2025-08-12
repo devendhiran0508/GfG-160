d1f56228e898f74666bba760096dec361cea126b
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-08-12 10:00:31 +0530

    Create Reverse an Array.py

---
+++ Arrays/Reverse an Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        n=len(arr)
+        for i in range(n//2):
+            temp=arr[i]
+            arr[i]=arr[n-1-i]
+            arr[n-1-i]=temp
+        return arr
+        
+        
+        
+        
