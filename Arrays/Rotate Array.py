25a5b2518ace07e26e7de7a2eb77cb7a73e8535f
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-08-18 22:25:35 +0530

    Create Rotate Array.py

---
+++ Arrays/Rotate Array.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        #Your code here
+        n=len(arr)
+        d%=n
+        reverse(arr,0,d-1)
+        reverse(arr,d,n-1)
+        reverse(arr,0,n-1)
+        
+def reverse(arr,start,end):
+    while start<end:
+        arr[start],arr[end]=arr[end],arr[start]
+        start+=1
+        end-=1
