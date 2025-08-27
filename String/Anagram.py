86c4b30219932fa279a06bd9c26165634efa6771
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-08-27 22:06:15 +0530

    Create Anagram.py

---
+++ String/Anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def areAnagrams(self, s1, s2):
+       # code here
+        if len(s1)!=len(s2):
+           return False
+        
+        charCount={}
+        for ch in s1:
+            charCount[ch]=charCount.get(ch,0)+1
+        for ch in s2:
+            charCount[ch]=charCount.get(ch,0)-1
+        
+        for val in charCount.values():
+            if val!=0:
+                return False
+        return True
+    
