25f546156b957dbcf43fc6bbc4bc84bbe9898ed4
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-07-05 22:27:22 +0530

    Implement reverseArray method in Solution class
    Added a Solution class with a method to reverse an array in place.

--- Arrays/Reverse an Array.py
+++ Arrays/Reverse an Array.py
@@ -8,6 +8,18 @@ class Solution:
             arr[n-1-i]=temp
         return arr
         
+///////////////////////////////
+
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        left = 0
+        right = len(arr) - 1
+        
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
         
         
         
