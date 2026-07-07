2af8dc8aec24fca5f5551c7e9677093bf7b12259
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-07-07 16:15:05 +0530

    Implement majority element finder for n/3

---
+++ Arrays/C++/Majority Element - More Than n by 3.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    vector<int> findMajority(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int ele1 = -1; int ele2 = -1;
+        int cnt1 = 0; int cnt2 = 0;
+        
+        for (int ele : arr) {
+            if (ele1 == ele) cnt1++;
+            else if (ele2 == ele) cnt2++;
+            else if (cnt1 == 0) {
+                ele1 = ele;
+                cnt1++;
+            }
+            else if (cnt2 == 0) {
+                ele2 = ele;
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        vector<int> res;
+        cnt1 = 0; cnt2 = 0;
+        
+        for (int ele : arr) {
+            if (ele1 == ele) cnt1++;
+            if (ele2 == ele) cnt2++;
+        }
+        
+        if (cnt1 > n / 3) res.push_back(ele1);
+        if (cnt2 > n / 3 && ele1 != ele2) res.push_back(ele2);
+        
+        if (res.size() == 2 && res[0] > res[1]) {
+            swap(res[0], res[1]);
+        }
+        return res;
+    }
+};
