fdfc9ce0e41e0eb80c9bfc8fc46a529d42dac5de
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-07-11 21:09:19 +0530

    Implement non-repeating character function

---
+++ String/C++/Non Repeating Character.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        //  code here
+        vector<int> freq(26,0);
+        
+        for (char c: s)
+            freq[c - 'a']++;
+        for (char c: s) {
+            if (freq[c - 'a'] == 1) 
+                return c;
+        }
+        return '\$';
+    }
+};
