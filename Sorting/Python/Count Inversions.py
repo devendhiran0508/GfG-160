29db22493efd80ebefaa4c09d9f0b192921dbb93
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-07-20 20:14:06 +0530

    Implement Count Inversions algorithm in Python

---
+++ Sorting/Python/Count Inversions.py
@@ -0,0 +1,42 @@
+class Solution:
+    def CountInv(self, arr, l ,r):
+        res = 0
+        if l < r:
+            m = (l + r) // 2
+            res += self.CountInv(arr, l, m)
+            res += self.CountInv(arr, m + 1, r)
+            res += self.CountAndMerge(arr, l, m, r)
+        return res
+    
+    def CountAndMerge(self, arr, l, m, r):
+        n1 = m - l + 1
+        n2 = r - m
+        left = arr[l : m + 1]
+        right = arr[m + 1 : r + 1]
+        res = 0
+        i = 0
+        j = 0
+        k = l
+        
+        while i < n1 and j < n2:
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+                res += (n1 - i)
+            k += 1
+        while i < n1:
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < n2:
+            arr[k] = right[j]
+            j += 1
+            k += 1
+        return res
+    
+    def inversionCount(self, arr):
+        # code here
+        return self.CountInv(arr, 0, len(arr) - 1)
