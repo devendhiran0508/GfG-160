5f809bdd094e8e3f966aa739d6cf0c538a65fb09
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-07-25 11:34:23 +0530

    Implement findMin for sorted and rotated array

---
+++ Searching/Python/Sorted and Rotated Minimum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMin(self, arr):
+        # code here
+        low = 0
+        high = len(arr) - 1
+        
+        while low < high:
+            if arr[low] < arr[high]:
+                return arr[low]
+            mid = low + (high - low) // 2
+            
+            if arr[mid] > arr[high]:
+                low = mid + 1
+            else:
+                high = mid
+        return arr[low]
+            
