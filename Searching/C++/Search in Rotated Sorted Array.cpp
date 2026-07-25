eddd3005e2698c1143f93ad5bd344cfc57282c93
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-07-25 21:30:07 +0530

    Implement search in rotated sorted array

---
+++ Searching/C++/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        // Code Here
+        int low = 0;
+        int high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] >= arr[low]) {
+                if (key >= arr[low] && key < arr[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (key > arr[mid] && key <= arr[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
