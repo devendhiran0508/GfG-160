d15999b8698804ba0a085ea1c5b0c05d51a6ea47
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2026-08-13 18:44:07 +0530

    Implement twoSum function to find pair with target sum

---
+++ Hashing/Python/C++/Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_set<int> s;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            int comp = target - arr[i];
+            
+            if (s.find(comp) != s.end())    
+                return true;
+            
+            s.insert(arr[i]);
+        }
+        return false;
+    }
+};
